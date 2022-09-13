639dce2e06cd6905f0b07aa72bff00dc8ec6c086
Virendra Singh Solanki <thevirendrapal@gmail.com>
2022-09-13 12:38:16 +0000

    Update 2 files

--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,16 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "chat-ab746.firebaseapp.com",
-  projectId: "chat-ab746",
-  storageBucket: "chat-ab746.appspot.com",
-  messagingSenderId: "901216368405",
-  appId: "1:901216368405:web:8ec942ee51611df5c49b1c",
+  apiKey: "AIzaSyAGj5xrJJwbZI4IYfQyJlUWtGVEjkngdI4",
+  authDomain: "reactchat-d795e.firebaseapp.com",
+  projectId: "reactchat-d795e",
+  storageBucket: "reactchat-d795e.appspot.com",
+  messagingSenderId: "678763210376",
+  appId: "1:678763210376:web:4ca2a6132d87e17a07a13d",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
-export const db = getFirestore()
+export const db = getFirestore();
